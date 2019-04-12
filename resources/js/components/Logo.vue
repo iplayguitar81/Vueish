f3825480d0fde08c16afe957ec847d8c10be0092
Chris Snyder <chris.snyder@triadhq.com>
2019-04-12 11:07:08 -0700

    updated routes file to accept any route past one directory...

--- resources/js/components/Logo.vue
+++ resources/js/components/Logo.vue
@@ -1,7 +1,24 @@
 <template>
 
-    <h1 class="font-normal text-3xl text-grey-darkest leading-none">Logo</h1>
+    <div>
+
+        <h1 class="font-normal text-3xl text-grey-darkest leading-none mb-4">Logo</h1>
+
+
+
+            <picture style="height: 382px;">
+                <img src="/images/autopsy.jpg" alt="aliens bro">
+            </picture>
+
+        <div class="flex justify-between">
+            <p>Alien autopsy.  The truth is out there brotha</p>
+
+            <a href="/images/autopsy.jpg" class="">autopsy.jpg</a>
+        </div>
+    </div>
 </template>
+
+
 
 <script>
     export default {
