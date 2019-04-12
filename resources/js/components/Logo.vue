79682bdc7c34bef914d97ef99d0be91e6539509d
Chris Snyder <chris.snyder@triadhq.com>
2019-04-12 14:24:02 -0700

    updated routes file to accept any route past one directory...

--- resources/js/components/Logo.vue
+++ resources/js/components/Logo.vue
@@ -2,20 +2,50 @@
 
     <div>
 
-        <h1 class="font-normal text-3xl text-grey-darkest leading-none mb-4">Logo</h1>
+        <h1 class="font-normal text-3xl text-grey-darkest leading-none mb-8">Logo</h1>
 
 
+        <div class="mb-10">
 
-            <picture style="height: 382px;">
-                <img src="/images/autopsy.jpg" alt="aliens bro">
+            <picture style="height: 382px;" class="bg-red-lighter">
+                <img src="/images/autopsy.jpg" alt="aliens bro" class="block shadow flex items-center justify-center mb-6 rounded-lg">
             </picture>
 
-        <div class="flex justify-between">
-            <p>Alien autopsy.  The truth is out there brotha</p>
+        <div class="flex justify-between items-center">
+            <p>Alien autopsy</p>
 
-            <a href="/images/autopsy.jpg" class="">autopsy.jpg</a>
+            <a href="/images/autopsy.jpg" class="border border-grey-light px-8 py-2 rounded-full text-black font-bold text-xs">autopsy.jpg</a>
         </div>
+
+        </div>
+
+
+
+
+        <h1 class="font-normal text-3xl text-grey-darkest leading-none mb-8">Logo</h1>
+
+
+        <div class="mb-10">
+
+            <picture style="height: 382px;" class="bg-blue">
+                <img src="/images/ufo.jpg" alt="ufos bro" class="block shadow flex items-center justify-center mb-6 rounded-lg">
+            </picture>
+
+            <div class="flex justify-between items-center">
+                <p>UFO</p>
+
+                <a href="/images/ufo.jpg" class="border border-grey-light px-8 py-2 rounded-full text-black font-bold text-xs">ufo.jpg</a>
+            </div>
+
+        </div>
+
+
     </div>
+
+
+
+
+
 </template>
 
 
